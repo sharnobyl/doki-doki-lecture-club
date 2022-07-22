2d3f936ae55ee4373bd602540dc2aeee15de9709
Youting <youting123456@gmail.com>
2022-07-23 11:27:35 +1200

    Set up the server and database

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express, { application } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/ddlc';
+
+mongoose.connect(DB_URL)
+    .then(() => {
+        console.log("Connected to db");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Running On Port ${PORT}`);
+})
