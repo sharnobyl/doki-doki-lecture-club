65c24dbcc59637d206c3f2a77392c4c10f9b901b
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-07-23 17:07:07 +1200

    fixing

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 import './sidebar.css'
 
 export default function Sidebar() {
@@ -16,40 +17,58 @@ export default function Sidebar() {
     }
 
     const addCourseName1 = () => {
-        setCourse1Clicked(!course1Clicked)
         if (course1Clicked) {
-
+            ReactDOM.render(`1. ${name}`, document.getElementById("btn1"));
         } else {
-
+            ReactDOM.render(`1`, document.getElementById("btn1"));
         }
+        setCourse1Clicked(!course1Clicked)
+
     }
 
     const addCourseName2 = () => {
+        if (course2Clicked) {
+            ReactDOM.render(`2. ${name}`, document.getElementById("btn2"));
+        } else {
+            ReactDOM.render(`2`, document.getElementById("btn2"));
+        }
         setCourse2Clicked(!course2Clicked)
+
     }
 
     const addCourseName3 = () => {
+        if (course3Clicked) {
+            ReactDOM.render(`3. ${name}`, document.getElementById("btn3"));
+        } else {
+            ReactDOM.render(`3`, document.getElementById("btn3"));
+        }
         setCourse3Clicked(!course3Clicked)
     }
 
     const addCourseName4 = () => {
+        if (course4Clicked) {
+            ReactDOM.render(`4. ${name}`, document.getElementById("btn4"));
+        } else {
+            ReactDOM.render(`4`, document.getElementById("btn4"));
+        }
         setCourse4Clicked(!course4Clicked)
+
     }
 
     return (
         <div className={clicked ? "sidebar menuOpen" : "sidebar"}>
             <div className={clicked ? "menuBtn menuBtnOpen" : "menuBtn"} onClick={addOpenMenuClass}></div>
             <div className='classCont' onClick={addCourseName1}>
-                <div className={clicked ? "classBtn classBtnOpen" : "classBtn"}>1</div>
+                <div id="btn1" className={clicked ? "classBtn classBtnOpen" : "classBtn"}>1</div>
             </div>
             <div className='classCont' onClick={addCourseName2}>
-                <div className={clicked ? "classBtn classBtnOpen" : "classBtn"}>2</div>
+                <div id="btn2" className={clicked ? "classBtn classBtnOpen" : "classBtn"}>2</div>
             </div>
             <div className='classCont' onClick={addCourseName3}>
-                <div className={clicked ? "classBtn classBtnOpen" : "classBtn"}>3</div>
+                <div id="btn3" className={clicked ? "classBtn classBtnOpen" : "classBtn"}>3</div>
             </div>
             <div className='classCont' onClick={addCourseName4}>
-                <div className={clicked ? "classBtn classBtnOpen" : "classBtn"}>4</div>
+                <div id="btn4" className={clicked ? "classBtn classBtnOpen" : "classBtn"}>4</div>
             </div>
         </div >
 
