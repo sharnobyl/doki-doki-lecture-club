2477c32c1e4285441b8975be750735666d97f39b
jrut644 <jrut644@aucklanduni.ac.nz>
2022-07-23 21:54:29 +1200

    fix randomisation

--- client/src/components/jumpscare/jumpscare.js
+++ client/src/components/jumpscare/jumpscare.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import './jumpscare.css'
 
+
+
+
 export default function Jumpscare() {
 
     const [isVisible, setVisible] = useState(false);
@@ -8,30 +11,45 @@ export default function Jumpscare() {
     function triggerScare() {
 
         var time = Math.floor(500*Math.random());
-
-        //document.getElementById('monica').src = "/images/just-monica-scare.png";
-
+    
+        if (Math.random() > 0.8) {
+            document.getElementById('monicaScare').src = "/images/just-monica-scare.png";
+        } else {
+            document.getElementById('monicaScare').src = "/images/just-monica.png";
+        }
+        
+    
         setVisible(true);
         
         setTimeout(function() {
             setVisible(false);
           }, time);
+    
+    }
 
-
+    function considerScare() {
+        if (Math.random() > 0.8) {
+            triggerScare();
+        }
     }
 
     return (
         <div className='jumpscare'>
+            <span className='clickArea' onClick={() => considerScare()}>
 
-            <div className='logo'>
-                <input type="image" src="/images/doki_doki_logo.png" alt="Logo" width="41" height="41" onClick={() => triggerScare()} />
-            </div>
+                <div className='logo'>
+                    <input type="image" src="/images/doki_doki_logo.png" alt="Logo" width="41" height="41" onClick={() => triggerScare()} />
+                </div>
 
 
-            <div className={isVisible ? 'visible' : 'hidden' }> 
-                <img src="/images/just-monica.png" id='monica' height='100%' width='100%' />
-            </div>
+                <div className={isVisible ? 'visible' : 'hidden' }> 
+                    <img src="/images/just-monica.png" id='monicaScare' height='100%' width='100%' />
+                </div>
+            </span>
+
+            
 
         </div>
     )
 }
+
