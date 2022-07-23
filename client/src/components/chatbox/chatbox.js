de78be6cd695d2a89fc0ab7ec4837109835b066b
luke-mccallum <luke.mccallum122@gmail.com>
2022-07-24 09:38:48 +1200

    foreach (does not work)

--- client/src/components/chatbox/chatbox.js
+++ client/src/components/chatbox/chatbox.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './chatbox.css'
 
 const ChatBox = () => {
+    const [messages, setMessages] = useState([
+        { id: 1, message: 'Austria' },
+        { id: 2, message: 'Germany' },
+        { id: 3, message: 'Austria' },
+    ]
+    )
+
+    const printMessage = () => {
+        messages.forEach((message) => {
+            React.createElement(
+                'li',
+                {},
+                { message }
+            )
+        })
+    }
+
     return (
-        <textarea className='chatbox'>
-           placeholder
-        </textarea>
+        <div className="chatbox">
+            <ul>{printMessage()}</ul>
+        </div>
     );
 }
 
