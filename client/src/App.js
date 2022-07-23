845192e1a0cd7af0a3d5cc797c705316418b3650
luke-mccallum <luke.mccallum122@gmail.com>
2022-07-23 13:45:18 +1200

    added test video

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import ChatBox from './components/chatbox/chatbox.js'
+import Video from './components/video/video.js'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         </h1>
       </header>
       <ChatBox />
+      <Video />
     </div>
   );
 }
---
+++ client/src/components/video/video.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Video = () => {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/nWtCTpJG6E4"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  );
+};
+
+export default Video;
