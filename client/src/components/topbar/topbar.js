ceff5fa730dcc287d8670b4f5c21fb1c4e90fb77
jrut644 <jrut644@aucklanduni.ac.nz>
2022-07-23 15:33:42 +1200

    add long part of logo to topbar

--- client/src/components/topbar/topbar.js
+++ client/src/components/topbar/topbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './topbar.css'
-// import logo from "./doki_doki_logo.png"; 
 
 export default function Topbar() {
     return (
         <div className='topbar'>
             <div className='logo'>
-                <img src="/images/doki_doki_logo.png" alt="Logo" />
+                <img src="/images/doki_doki_logo.png" alt="Logo" width="41" height="41"/>
+                <img src="/images/doki_long_logo.png" alt="Logo"/>
             </div>
         </div>
     )
