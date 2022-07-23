f6943e92d554a00f4a6cf8a4357b54ac1f1767d6
luke-mccallum <luke.mccallum122@gmail.com>
2022-07-23 14:16:08 +1200

    video proof of concept implemented

--- client/src/components/video/video.js
+++ client/src/components/video/video.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Video = () => {
-  return (
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/nWtCTpJG6E4"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    ></iframe>
+function Video() {
+  const [display, setDisplay] = useState(
+    "https://www.youtube.com/embed/Ev4_YB4HDrc"
   );
-};
+
+  function changeDisplay() {
+    setDisplay("https://www.youtube.com/embed/nWtCTpJG6E4");
+  }
+
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={display}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <button onClick={() => changeDisplay()}>change video</button>
+    </div>
+  );
+}
 
 export default Video;
