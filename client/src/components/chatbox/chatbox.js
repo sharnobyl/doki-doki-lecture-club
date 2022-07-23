3ae72f138eb3681e4bf31d05b364c20e0a808861
luke-mccallum <luke.mccallum122@gmail.com>
2022-07-24 10:42:02 +1200

    helper changes

--- client/src/components/chatbox/chatbox.js
+++ client/src/components/chatbox/chatbox.js
@@ -2,28 +2,34 @@ import React, { useState , useEffect} from 'react'
 import './chatbox.css'
 
 const ChatBox = () => {
-    const [messages, setMessages] = useState([
+    const messages = [
         { id: 1, message: 'Austria', user: '232'},
         { id: 2, message: 'Germany', user: '24322' },
         { id: 3, message: 'Austria', user: '235322' },
     ]
-    )
-    const [listElements, setListElements] = useState([React.createElement('li',{},'chat start')])
+
+    // const [listElements, setListElements] = useState([React.createElement('li',{},'chat start')])
 
     useEffect(
         () => {
         messages.forEach((message) => {
-            setListElements({listElements : [...listElements, React.createElement('li',{}, message)]})
+            // setListElements({listElements : [...listElements, React.createElement('li',{}, message)]})
         })
     }
     )
 
+    const displayMessage = (message) => {
+        return(
 
-    return (
-        <div className="chatbox">
-            <ul>{listElements}</ul>
-        </div>
-    );
+            <p>{message.message}</p>
+        )
+    }
+
+
+    return 
+    <div>
+        {messages.map(m => displayMessage(m))}
+    </div>;
 }
 
 export default ChatBox
