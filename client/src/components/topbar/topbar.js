34ea2d2e246d0092d64d170551de7ba200b84b83
jrut644 <jrut644@aucklanduni.ac.nz>
2022-07-23 15:00:41 +1200

    insert logo in topbar

--- client/src/components/topbar/topbar.js
+++ client/src/components/topbar/topbar.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './topbar.css'
+// import logo from "./doki_doki_logo.png"; 
 
 export default function Topbar() {
     return (
-        <div className='topbar'>topbar</div>
+        <div className='topbar'>
+            <div className='logo'>
+                <img src="/images/doki_doki_logo.png" alt="Logo" />
+            </div>
+        </div>
     )
 }
