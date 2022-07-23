76b0a13db7b265d859ec0d1d73f5bd8390ed0ea3
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-07-23 14:16:27 +1200

    sidebar trying to fix

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,7 @@ import Video from './components/video/video.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          doki doki dunking on u
-        </h1>
-      </header>
-
-      <Sidebar className="sidebar"></Sidebar>
+      <Sidebar />
       <Video />
     </div>
   );
