642b02b59d239e256f339aad38b19c55fcde7cb3
luke-mccallum <luke.mccallum122@gmail.com>
2022-07-24 10:55:44 +1200

    added database reading

--- client/src/components/chatbox/chatbox.js
+++ client/src/components/chatbox/chatbox.js
@@ -1,12 +1,23 @@
 import React, { useState , useEffect} from 'react'
 import './chatbox.css'
+import axios from 'axios'
 
-const ChatBox = () => {
-    const messages = [
-        { time: 1, message: 'Austria', user: '232'},
-        { time: 2, message: 'Germany', user: '24322' },
-        { time: 3, message: 'Austria', user: '235322' },
-    ]
+const ChatBox = (props) => {
+    const [messages, setMessages] = useState([
+        {
+          messages: [{ time: 1, message: 'Austria', user: '232'}, 
+          { time: 2, message: 'Germany', user: '24322' },
+          { time: 3, message: 'Austria', user: '235322' }]
+        },
+      ]);
+
+    useEffect(
+        () => {
+            axios.get("http://localhost:5000/lecture").then(function (response) {
+                setMessages(response.data[props.selectedVideo].comments)
+            });
+          }, []
+      );
 
     const displayMessage = (message) => {
         return (
