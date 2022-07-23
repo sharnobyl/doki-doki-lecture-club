6bad2b30bd13a8b8c1443fa7920d855fa33596a8
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-07-23 15:00:17 +1200

    fuckup

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -2,10 +2,17 @@ import React, { useState } from 'react'
 import './sidebar.css'
 
 export default function Sidebar() {
+
+    const [clicked, setClicked] = useState(false);
+
+    const addOpenMenuClass = () => {
+        setClicked(true);
+    }
+
     return (
-        <div className="side-bar">
-            <div className='menuBtn'></div>
-        </div>
+        <div className={clicked ? "sidebar menuOpen" : "sidebar"}>
+            <div className="menuBtn" onClick={addOpenMenuClass}></div>
+        </div >
 
     )
 }
