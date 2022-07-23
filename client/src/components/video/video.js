dbb54c3ac6323a50e395bbbb5fa6971c434d39e4
luke-mccallum <luke.mccallum122@gmail.com>
2022-07-23 19:33:36 +1200

    dog doin

--- client/src/components/video/video.js
+++ client/src/components/video/video.js
@@ -1,30 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./video.css";
+import axios from "axios";
 
-function Video() {
+function Video(id) {
   const [display, setDisplay] = useState(
     "https://www.youtube.com/embed/Ev4_YB4HDrc"
   );
   const [name, setName] = useState("ayo what the dog doing");
   const [subject, setSubject] = useState("life");
+  const [lectureData, setLectureData] = useState([{
+    lectureLink: "https://www.youtube.com/embed/gPPxfPThq20"
+  }]);
 
   function changeDisplay() {
     if (display === "https://www.youtube.com/embed/nWtCTpJG6E4") {
       setDisplay("https://www.youtube.com/embed/Ev4_YB4HDrc");
-      setName("ayo what the dog doin?")
-      setSubject("life")
+      setName("ayo what the dog doin?");
+      setSubject("life");
     } else {
       setDisplay("https://www.youtube.com/embed/nWtCTpJG6E4");
-      setName("some 284 shit")
-      setSubject("SOFTENG 284")
+      setName("some 284 shit");
+      setSubject("SOFTENG 284");
     }
   }
+
+  useEffect(
+    () =>
+      function fetchData() {
+        axios.get("http://localhost:5000/lecture").then(function (response) {
+          setLectureData(response.data);
+          console.log(lectureData)
+          console.log(lectureData[0].lectureLink)
+        });
+      }
+  );
 
   return (
     <div>
       <div class="videoplayer">
         <iframe
-          src={display}
+          src={lectureData[0].lectureLink}
           frameBorder="0"
           title="YouTube video player"
           height="315"
