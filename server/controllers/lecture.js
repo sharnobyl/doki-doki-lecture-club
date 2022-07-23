9ee735ccb27455707548c13fc6a6411acebd63bc
Youting Lei <94492809+youtinglei@users.noreply.github.com>
2022-07-23 20:52:17 +1200

    Removed annoying print

--- server/controllers/lecture.js
+++ server/controllers/lecture.js
@@ -1,28 +1,23 @@
-import mongoose from 'mongoose';
-import Lecture from '../models/lecture.js';
+import mongoose from "mongoose";
+import Lecture from "../models/lecture.js";
 
 export const getLectures = async (req, res) => {
-    
-    try {
-        const subjects = await Lecture.find({});
-        console.log(subjects);
-        res.json(subjects);
-    } catch (error) {
-        res.status(200).json({ message: error.message });
-    }
-}
+  try {
+    const subjects = await Lecture.find({});
+    res.json(subjects);
+  } catch (error) {
+    res.status(200).json({ message: error.message });
+  }
+};
 
-export const createSubject = async (req, res) => {
-}
+export const createSubject = async (req, res) => {};
 
-export const deleteSubject = async (req, res) => {
-
-}
+export const deleteSubject = async (req, res) => {};
 
 export const addLectureToSubject = async (req, res) => {
-    // PATCH REQUEST
-}
+  // PATCH REQUEST
+};
 
 export const deleteLectureFromSubject = async (req, res) => {
-    // PATCH REQUEST (Because we are updating the lectures list in subjects)
-}
+  // PATCH REQUEST (Because we are updating the lectures list in subjects)
+};
