acc6ab44429a79a89270b7a01db0831c48e92462
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-07-23 20:14:39 +1200

    take care sharjil

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
+import { useState } from 'react';
 // import ChatBox from './components/chatbox/chatbox.js'
 import Sidebar from './components/sidebar/sidebar';
 import Video from './components/video/video.js'
 import Topbar from './components/topbar/topbar';
 
 function App() {
+
+  const [selectedVideo, setSelectedVideo] = useState(1);
+
+  const toggleVideo = (videoId) => {
+    setSelectedVideo(videoId)
+  }
+
   return (
     <div className="App">
-      <Sidebar />
-      <Video />
+
+      <Sidebar toggleVideo={toggleVideo} />
+      <Video selectedVideo={selectedVideo} />
       <Topbar />
     </div>
-    
+
   );
 }
 
